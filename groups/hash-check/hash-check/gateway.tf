1dbab1635ed9378c0d03c2ea3f3372647efd70bb
amartin7663 <almartin@companieshouse.gov.uk>
2021-06-14 14:36:07 +0100

    add response model to method response

--- groups/hash-check/hash-check/gateway.tf
+++ groups/hash-check/hash-check/gateway.tf
@@ -57,6 +57,9 @@ resource "aws_api_gateway_method_response" "response_200" {
   resource_id = aws_api_gateway_resource.hash_check_resource.id
   http_method = aws_api_gateway_method.hash_check_method.http_method
   status_code = "200"
+  response_models = {
+    "application/json" = ""
+  }
 }
 
 resource "aws_api_gateway_integration_response" "hash_check_integration_response" {
