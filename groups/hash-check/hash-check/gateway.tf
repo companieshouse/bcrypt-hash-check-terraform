379412b2c7fac230c5b76ad5c8c2c35420f1a1b3
amartin7663 <almartin@companieshouse.gov.uk>
2021-06-14 15:01:26 +0100

    add response template to integration response

--- groups/hash-check/hash-check/gateway.tf
+++ groups/hash-check/hash-check/gateway.tf
@@ -67,6 +67,9 @@ resource "aws_api_gateway_integration_response" "hash_check_integration_response
   resource_id = aws_api_gateway_resource.hash_check_resource.id
   http_method = aws_api_gateway_method.hash_check_method.http_method
   status_code = aws_api_gateway_method_response.response_200.status_code
+  response_templates = {
+    "application/json" = ""
+  }
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
